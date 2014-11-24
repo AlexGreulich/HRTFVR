dc4c748a973cc54207ec1b97c8e815240513634a
Moritz Beller <beller.moritz@googlemail.com>
2014-11-24 16:03:30 +0100

    rendering first .obj file: a little bit distorted but ok Oo

--- HRTFVR/HRTFVR/VRmain.cpp
+++ HRTFVR/HRTFVR/VRmain.cpp
@@ -98,33 +98,10 @@ int main(void)
 		//Clear color buffer  
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		float points[] = {
-			0.0f, 0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			-0.5f, -0.5f, 0.0f
-		};
+		// 1 -> hardcoded triangle.obj
+		glBindVertexArray(1);
 
-		GLuint vbo = 0;
-		glGenBuffers(1, &vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
-
-		GLuint vao = 0;
-		glGenVertexArrays(1, &vao);
-		glBindVertexArray(vao);
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-
-		glBindVertexArray(vao);
-		// draw points 0-3 from the currently bound VAO with current in-use shader
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		// update other events like input handling 
-		glfwPollEvents();
-		// put the stuff we've been drawing onto the display
-		glfwSwapBuffers(window);
-		
-
+		glDrawArrays(GL_TRIANGLES, 0, 4);
 
 		/*
 
